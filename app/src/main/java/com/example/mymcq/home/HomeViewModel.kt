30b21557fcde9322765466249277a3d2f0f5124b
leslinantony <40162134+leslinantony@users.noreply.github.com>
2022-12-09 14:42:52 +0530

    Update HomeViewModel.kt

--- app/src/main/java/com/example/mymcq/home/HomeViewModel.kt
+++ app/src/main/java/com/example/mymcq/home/HomeViewModel.kt
@@ -18,12 +18,12 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
 
         var questions = listOf(
-            Questions(1, "what is 1+1", "1", "2", "3", "1", "2"),
-            Questions(2, "what is 2+3", "1", "2", "5", "2", "5"),
+            Questions(1, "what is 1+1", "1", "2", "3", "6", "2"),
+            Questions(2, "what is 2+3", "1", "2", "5", "26", "5"),
             Questions(3, "what is 3+3", "1", "6", "3", "33", "6"),
             Questions(4, "what is 4+3", "1", "7", "3", "5", "7"),
             Questions(5, "what is 5+3", "1", "8", "3", "7", "8"),
-            Questions(6, "what is 6+3", "9", "2", "3", "3", "9"),
+            Questions(6, "what is 6+3", "9", "2", "3", "8", "9"),
             Questions(7, "what is 7+3", "1", "2", "3", "7", "7"),
             Questions(8, "what is 8+3", "11", "2", "3", "8", "11"),
             Questions(9, "what is 9+3", "12", "11", "3", "6", "12"),
